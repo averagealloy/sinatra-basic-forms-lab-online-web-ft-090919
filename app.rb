7f66c1c50d1ac0756a52e435b0506046d56c0f11
Mike McTighe <m.mctighe10@gmail.com>
2019-10-24 17:59:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :index
   end
@@ -9,8 +10,9 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-post '/puppy' do
-@puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
-  erb :display_puppy
-end
+  post '/puppy' do
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
+    erb :display_puppy
+  end
+
 end
